781f6c587ba3434d6e226fd9c043486f331a4bb7
Phreelosu <siege.team22@gmail.com>
2023-09-05 12:07:53 +0200

    Folytatás1

--- src/App.java
+++ src/App.java
@@ -1,13 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Maci maci = new Maci();
-        Macska mici = new Macska();
-        maci.hangotad("Brumm...");
-        maci.mezgyujtes();
-
-        mici.hangotad("miauuu...");
-        mici.dorombol();
-        mici.beallitNev("");
-
+        Kert kert = new Kert();
+        kert.inditElet();
     }
 }
---
+++ src/Kert.java
@@ -0,0 +1,13 @@
+public class Kert {
+    public  void inditElet() {
+        Maci maci = new Maci();
+        Macska mici = new Macska();
+        maci.hangotad("Brumm...");
+        maci.mezgyujtes();
+
+        mici.hangotad("miauuu...");
+        mici.dorombol();
+        mici.beallitNev("");
+
+    }
+}
